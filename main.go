ecea7e59201df341696f373ad0bd5087a5f38b7c
Nathan G. Santos <nathangar01@gmail.com>
2025-03-24 21:18:18 -0300

    feat: list all running containers

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	apiClient, err := client.NewClientWithOpts(client.WithVersion("1.41"), client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+	defer apiClient.Close()
+
+	containers, err := apiClient.ContainerList(context.Background(), container.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, ctr := range containers {
+		fmt.Printf("%s (status: %s)\n", ctr.Image, ctr.Status)
+	}
 }
